f64d806206b605f5c9c8952757e59b2ea17f333e
paul4912 <paulyang4912@gmail.com>
2023-10-25 17:00:27 -0700

    Interest Accure Function

--- src/Emu.sol
+++ src/Emu.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.20;
 
 struct SliceData {
+  //pack struct?
   uint256 depositIndex;
   uint256 totalBaseDeposit;
   uint256 debtIndex;
   uint256 totalBaseDebt;
+  uint256 depositEpoch;
   uint256 lastUpdate;
 }
 
@@ -17,21 +19,34 @@ contract Emu {
 
   uint256[] public createdSlices; // Array of prices of slices that have been creted
 
+  uint256 public claimableCollateralIndex;
+  uint256 public claimableDebtTokenIndex;
+
   mapping(uint256 price => SliceData sliceData) private slices;
   mapping(address user => mapping(uint256 slice => uint256 baseAmount)) private
     lenderDeposits;
+  mapping(address user => mapping(uint256 slice => uint256 epoch)) private lenderEpoch;
   mapping(address user => mapping(uint256 slice => uint256 baseAmount)) private
     borrowerDebts;
   mapping(address user => mapping(uint256 slice => uint256 amount)) private
     collateralDeposits;
+  mapping(address user => mapping(uint256 slice => uint256 amount)) private
+    userClaimableCollateralIndex;
+  mapping(address user => mapping(uint256 slice => uint256 amount)) private
+    userClaimableDebtTokenIndex;
 
   function _accureInterest(uint256 _slice) internal {
     SliceData storage slice = slices[_slice];
+
     uint256 timePassedSinceLastUpdate = block.timestamp - slice.lastUpdate;
     uint256 totalDebt = _toNominal(slice.totalBaseDebt, slice.debtIndex);
     uint256 interestAccuredPerYear = totalDebt * interestRateBPS / BPS;
-    uint256 interestAccured = interestAccuredPerYear / secondsInYear;
-    uint256 newTotalDebt = totalDebt + interestAccured;
+    uint256 interestAccured =
+      interestAccuredPerYear * timePassedSinceLastUpdate / secondsInYear;
+
+    slice.debtIndex += interestAccured * RAY / slice.totalBaseDebt;
+    slice.depositIndex += interestAccured * RAY / slice.totalBaseDeposit;
+    slice.lastUpdate = block.timestamp;
   }
 
   function _toNominal(uint256 _baseAmount, uint256 _index)
