6b9275f03b29527d2595a0408ce0167a2b2117e2
paul4912 <paulyang4912@gmail.com>
2023-11-06 10:47:06 -0800

    uniswap stuff so far

--- src/Emu.sol
+++ src/Emu.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { FullMath } from "./library/FullMath.sol";
+import { FullMath } from "./libraries/FullMath.sol";
 import {
   SliceData,
   ClaimableData,
--- src/EmuV1.sol
+++ src/EmuV1.sol
@@ -1,7 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { LiquidityAmounts } from
-  "@uniswap/v3-periphery/contracts/libraries/LiquidityAmounts.sol";
+import { LiquidityAmounts } from "./libraries/uniswap/LiquidityAmounts.sol";
+import { INonfungiblePositionManager } from
+  "./interface/uniswap/INonfungiblePositionManager.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract EmuV1 { }
+contract EmuV1 {
+  ERC20 public COLLATERAL_TOKEN;
+  ERC20 public DEBT_TOKEN;
+  INonfungiblePositionManager public _uniV3Manager;
+}
